d45034019d198d32c14c6d152ddb1d8a5bf1b6d0
cuhionfalls <np03cs4s250041@heraldcollege.edu.np>
2026-02-01 10:42:12 +0545

    last change

---
+++ admin/edit.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Edit Courses</title>
+</head>
+<body>
+    <div class="container">
+        <div class="left_side">
+            <ul>
+                <ul><a href="add_course.php">Add Course & Assign Teacher</a></ul>
+                <ul><a href="add_teacher.php">Add Teacher</a></ul>
+                <ul><a href="edit.php">Manage Courses & Teachers</a></ul>
+                <ul><a href="manage_students.php">Manage Students</a></ul>
+                <ul><a href="delete.php">Delete Data</a></ul>
+                <ul><a href="../auth/logout.php">Logout</a></ul>
+            </ul>
+        </div>
+        <div class="right_side">
+            <h2>Manage Courses & Teachers</h2>
+            <?php
+            
+            ?>
+        </div>
+    </div>
+</body>
+</html>
